c057dc6e8f55c99b38c9a4176e7e12117137ce0c
PavelBazilevich <102550003+PavelBazilevich@users.noreply.github.com>
2022-08-25 17:12:04 +0300

    add task 1, 2

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
-let hello = '';
+const pEl = document.body.firstElementChild;
+const buttonStart = pEl.nextElementSibling;
+const buttonStop = buttonStart.nextElementSibling;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+buttonStart.addEventListener('click', changeColor);
+buttonStop.addEventListener('click', stopChangeColor);
+
+function changeColor(e) {
+  buttonStop.disabled = false;
+  changeColorId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  buttonStart.disabled = true;
+}
+
+function stopChangeColor(e) {
+  buttonStart.disabled = false;
+  clearInterval(changeColorId);
+  buttonStop.disabled = true;
+}
