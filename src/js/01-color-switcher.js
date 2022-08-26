7e2508b1fc9867b2f719d4d90ed0601c334e4255
PavelBazilevich <102550003+PavelBazilevich@users.noreply.github.com>
2022-08-27 00:00:14 +0300

    update color-swicher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -6,21 +6,26 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
+
 buttonStop.disabled = true;
 
-const changeColor = e => {
-  buttonStop.disabled = false;
+const getInterval = () => {
   timerId = setInterval(() => {
     document.body.style.backgroundColor = getRandomHexColor();
   }, 1000);
-  buttonStart.disabled = true;
 };
 
-const stopChangeColor = e => {
+const changeColor = (e) => {
+  buttonStop.disabled = false;
+  getInterval();
+  buttonStart.disabled = true;
+}
+
+const stopChangeColor = (e) => {
   buttonStart.disabled = false;
   clearInterval(timerId);
   buttonStop.disabled = true;
-};
+}
 
 buttonStart.addEventListener('click', changeColor);
 buttonStop.addEventListener('click', stopChangeColor);
