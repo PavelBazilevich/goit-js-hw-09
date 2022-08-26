968085e5a1358e00a592c65e29e1cd88ae2fead5
PavelBazilevich <102550003+PavelBazilevich@users.noreply.github.com>
2022-08-26 20:19:54 +0000

    Deploying to gh-pages from @ PavelBazilevich/goit-js-hw-09@928e961d75c19b24853ff5bf87df0ba7f757aae2 🚀

---
+++ 01-color-switcher.93f60a5a.js
@@ -0,0 +1,2 @@
+!function(){var n=document.body.firstElementChild.nextElementSibling,e=n.nextElementSibling;e.addEventListener("click",(function(t){n.disabled=!1,clearInterval(changeColorId),e.disabled=!0})),n.addEventListener("click",(function(t){e.disabled=!1,changeColorId=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3),n.disabled=!0}))}();
+//# sourceMappingURL=01-color-switcher.93f60a5a.js.map
